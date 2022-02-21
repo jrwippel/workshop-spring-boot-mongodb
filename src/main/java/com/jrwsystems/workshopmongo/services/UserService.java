94f40cceed5a39ac6647968703f9c3a6ba304344
Jackson R Wippel <jrwippel@hotmail.com>
2022-02-21 14:52:48 -0300

    Delete of User

--- src/main/java/com/jrwsystems/workshopmongo/services/UserService.java
+++ src/main/java/com/jrwsystems/workshopmongo/services/UserService.java
@@ -33,4 +33,9 @@ public class UserService {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
+    public void delete (String id){
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
 }
